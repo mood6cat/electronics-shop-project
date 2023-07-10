ac1695eb0b69722d13a4e3f49df5a00d7751e083
dant9 <dant982@mail.ru>
2023-07-11 01:49:24 +0500

    TypeError: unsupported operand type(s) for +: 'Item' and 'Phone'

--- src/phone.py
+++ src/phone.py
@@ -2,19 +2,19 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, sim_cnt: int):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.__sim_cnt = sim_cnt
+        self.__number_of_sim = number_of_sim
 
     @property
-    def sim_cnt(self):
-        return self.__sim_cnt
+    def number_of_sim(self):
+        return self.__number_of_sim
 
-    # @sim_cnt.setter
+    # @number_of_sim.setter
     # def sim_cnt(self):
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.__name}, {self.price}, {self.quantity}, {self.sim_cnt}')"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
         # "Phone('iPhone 14', 120000, 5, 2)"
 
     def __add__(self, other):
